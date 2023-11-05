f957987a97112ea56f958ed9d7b85f12e62180bd
TimAde18 <aoadewole18@gmail.com>
2023-11-05 17:00:02 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCardComponent } from './add-card/add-card.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { HeaderComponent } from './header/header.component';
+import { TaskCardsComponent } from './task-cards/task-cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCardComponent,
+    AddTaskComponent,
+    HeaderComponent,
+    TaskCardsComponent
   ],
   imports: [
     BrowserModule,
